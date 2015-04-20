4c342ad079e4ef5536317e79b693d10c026b0894
Tom Hulihan <hulihan.tom159@gmail.com>
2015-04-20 09:08:35 -0400

    Cleanup the IO file

--- js/tabular/io.js
+++ js/tabular/io.js
@@ -1,17 +1,46 @@
 import Aviator from 'aviator';
 import { get as getCookie } from 'cookies';
+import { stringify as toJSON } from 'json';
 import { createElement, render } from 'react';
-import reqwest from 'reqwest';
-
+import { reqwest as request } from 'reqwest';
 import { ROOT_ELEMENT_ID, SESSION_KEY_COOKIE } from 'tabular/constants';
 
 /**
  * This module exports the IO functions required by the controllers.
  */
 export default {
-  getSessionKey: () => getCookie(SESSION_KEY_COOKIE),
-  navigate: (route, params) => Aviator.navigate(route, params),
-  request: reqwest,
+  /**
+   * Get the session key for the logged in user.
+   */
+  getSessionKey() {
+    return getCookie(SESSION_KEY_COOKIE);
+  },
+
+  /**
+   * Navigate to the given route.
+   */
+  navigate(route, params) {
+    return Aviator.navigate(route, params);
+  },
+
+  /**
+   * Send an HTTP request.
+   */
+  request(options) {
+    if (typeof options.data === 'object') {
+      options.data = toJSON(options.data);
+    }
+
+    options.headers = options.headers || {};
+    options.headers['Content-Type'] = 'application/json';
+    options.type = 'json';
+
+    return request(options);
+  },
+
+  /**
+   * Render the given component at the specified DOM node.
+   */
   render(component, params, callback) {
     const element = createElement(component, params);
     const node = document.getElementById(ROOT_ELEMENT_ID);
