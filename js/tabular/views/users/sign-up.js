775149f37fd8934f64842ec88ec26b1d88fbc560
Tom Hulihan <hulihan.tom159@gmail.com>
2015-04-20 10:49:11 -0400

    Refactor signup page

--- js/tabular/views/users/sign-up.js
+++ js/tabular/views/users/sign-up.js
@@ -15,10 +15,16 @@ class SignUp extends Component {
     super(props);
   }
 
-  createUser() {
+  /**
+   * IO calls (from the props).
+   */
+
+  createUser(event) {
     const username = document.getElementById(USERNAME_ID).value;
     const password = document.getElementById(PASSWORD_ID).value;
     const confirmation = document.getElementById(CONFIRMATION_ID).value;
+
+    event.preventDefault();
 
     this.props.createUser(username, password, confirmation, {
       success(data) {
@@ -31,50 +37,59 @@ class SignUp extends Component {
     });
   }
 
-  render() {
-    const component = this;
+  /**
+   * Render helpers.
+   */
 
-    const buttonAttributes = {
-      onClick(event) {
-        event.preventDefault();
-        component.createUser();
-      }
-    };
-
-    const tree =
+  formGroup() {
+    return (
       form({ className: 'pure-form pure-form-aligned' },
-        fieldset({},
-          div({ className: 'pure-control-group' },
-            label({
-              htmlFor: 'username'
-            }, 'Username'),
-            input({
-              id: USERNAME_ID,
-              type: 'text',
-              placeholder: 'Username'
-            })),
-          div({ className: 'pure-control-group' },
-            label({
-              htmlFor: 'password'
-            }, 'Password'),
-            input({
-              id: PASSWORD_ID,
-              type: 'password',
-              placeholder: '******'
-            })),
-          div({ className: 'pure-control-group' },
-            label({
-              htmlFor: 'password-confirmation'
-            }, 'Password Confirmation'),
-            input({
-              id: CONFIRMATION_ID,
-              type: 'password',
-              placeholder: '******'
-            })),
-          div({ className: 'pure-control-group' },
-            button(buttonAttributes, 'Submit'))));
+        fieldset({}, Array.prototype.slice.apply(arguments)))
+    );
+  }
 
-    return tree;
+  usernameField() {
+    return (
+      div({ key: 'username', className: 'pure-control-group' },
+        label({ htmlFor: 'username' }, 'Username'),
+        input({ id: USERNAME_ID, type: 'text', placeholder: 'Username' }))
+    );
+  }
+
+  passwordField() {
+    return (
+      div({ key: 'password', className: 'pure-control-group' },
+        label({ htmlFor: 'password' }, 'Password'),
+        input({ id: PASSWORD_ID, type: 'password', placeholder: '******' }))
+    );
+  }
+
+  confirmationField() {
+    return (
+      div({ key: 'confirmation', className: 'pure-control-group' },
+        label({ htmlFor: 'password-confirmation' }, 'Password Confirmation'),
+        input({
+          id: CONFIRMATION_ID,
+          type: 'password',
+          placeholder: '******'
+        }))
+    );
+  }
+
+  buttonField() {
+    return (
+      div({ key: 'button', className: 'pure-control-group' },
+        button({ onClick: (event) => this.createUser(event) }, 'Submit'))
+    );
+  }
+
+  render() {
+    return this.formGroup(
+      this.usernameField(),
+      this.passwordField(),
+      this.confirmationField(),
+      this.buttonField()
+    );
   }
 }
 
