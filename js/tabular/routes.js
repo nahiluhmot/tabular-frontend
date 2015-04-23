dbd9e148769e15107740c3f52cd10095d917e647
Tom Hulihan <hulihan.tom159@gmail.com>
2015-04-23 10:52:13 -0400

    Remove the users and sessions controllers

--- js/tabular/routes.js
+++ js/tabular/routes.js
@@ -1,7 +1,5 @@
 import { LINKS } from 'tabular/constants';
 import Root from 'tabular/controllers/root';
-import Sessions from 'tabular/controllers/sessions';
-import Users from 'tabular/controllers/users';
 import IO from 'tabular/io';
 
 const { home, login, logout, signUp } = LINKS;
@@ -12,13 +10,4 @@ export default {
   [login]: 'login',
   [logout]: 'logout',
   [signUp]: 'signUp',
-  '/users': {
-    target: new Users(IO),
-    '/create': 'signUp',
-    '/edit': 'changePassword'
-  },
-  '/sessions': {
-    target: new Sessions(IO),
-    '/create': 'login'
-  }
 };
