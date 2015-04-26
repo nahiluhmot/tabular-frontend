45699218e810cf5cbbc1ea0377bd3a7d048be368
Tom Hulihan <hulihan.tom159@gmail.com>
2015-04-26 09:27:17 -0400

    Add the relationships view

---
+++ js/views/pages/users/relationships.js
@@ -0,0 +1,47 @@
+import { Component, createElement, DOM } from 'vendored/react';
+
+import NavBar from 'views/navs/nav-bar';
+
+const { a, div, h1 } = DOM;
+
+/**
+ * This class represents search field and button for tabs.
+ */
+class Relationships extends Component {
+  /**
+   * Create a new Search field.
+   *
+   * Props:
+   *   - type:     The type of relationships, either 'following' or 'followers'.
+   *   - username: Unique identifier of the user whose relationships are being
+   *               viewed.
+   *   - users:    The users associated with the given usernameh.
+   */
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { type, username, users } = this.props;
+
+    const tree =
+      div({},
+        createElement(NavBar, this.props),
+        div({ className: 'container center' },
+          h1({},
+            'Users ',
+            (type === 'following') ? 'following ' : 'followed by ',
+            a({ className: 'navigate', href: `/u/${username}/` },
+              username)),
+          div({ className: 'list-group' },
+            users.map(({ username }) =>
+              a({
+                key: username,
+                className: 'list-group-item navigate',
+                href: `/u/${username}/`
+              }, username)))));
+    return tree;
+  }
+}
+
+export default Relationships;
